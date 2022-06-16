b8da83f9344e346e6d8611cac2a106cc582078ec
Cynthia Castillo <ca.castillo@gmail.com>
2022-06-16 09:53:27 -0500

    Tiempo Dojo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function alerta(ciudad) {
+    alert("Cargando informe meteorológico "+ciudad+"...");
+}
+
+function aceptar() {
+    var cookie = document.querySelector(".cookie"); //Seleccionando la etiqueta con clase cookie
+
+    //document.getElementById('IDENTIFICADOR');
+    
+    cookie.remove();
+}
